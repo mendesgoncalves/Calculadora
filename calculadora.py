6a11d724f6305144ee810c6cd5742378e1575a06
Mendes Gonçalves <88213337+mendesgoncalves@users.noreply.github.com>
2021-08-29 14:35:21 +0100

    Update calculadora.py
    Gosto de funções

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,17 @@
-
+def soma(n1,n2):
+    return n1+n2
+def sub(n1,n2):
+    return n1-n2
+def mult(n1,n2):
+    return n1*n2
+def div(n1,n2):
+    return n1/n2
 print("================================")
 print("\t CALCULADORA ")
 print("================================")
-
-n1=int(input("Digite o primeiro valor: "))
-n2=int(input("Digite o segundo valor: "))
-
-print("A soma dos dois valores é: {}".format(n1+n2))
-print("A subtração dos dois valores é: {}".format(n1-n2))
-print("O produto dos dois valores é: {}".format(n1*n2))
-print("A divisão dos dois valores é: {}".format(n1/n2))
+n1=int(input('Digite o 1 valores'))
+n2=int(input('Digite o 2 valores'))
+print("A soma dos dois valores é: ", soma(n1,n2))
+print("A subtração dos dois valores é: ",sub(n1-n2))
+print("O produto dos dois valores é: {}",mult(n1*n2))
+print("A divisão dos dois valores é: {}",div(n1/n2))
