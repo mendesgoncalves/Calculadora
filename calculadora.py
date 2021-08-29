b59e7ec878173e47f9b501e09a5be633f591cbbd
Simao Miguel <89701378+simaomiguel@users.noreply.github.com>
2021-08-29 14:18:36 +0100

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+
+print("================================")
+print("\t CALCULADORA ")
+print("================================")
+
+n1=int(input("Digite o primeiro valor: "))
+n2=int(input("Digite o segundo valor: "))
+
+print("A soma dos dois valores é: {}".format(n1+n2))
+print("A subtração dos dois valores é: {}".format(n1-n2))
+print("O produto dos dois valores é: {}".format(n1*n2))
+print("A divisão dos dois valores é: {}".format(n1/n2))
